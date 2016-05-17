7d5f6b9265d15c70e336fd3b48e538f62ecf578a
Guruprasad Kulkarni <guru@linux.com>
2016-05-17 09:12:35 +0530

    Updated Javadocs for for loop

--- src/test/java/com/rhcloud/test/forloop/ForLoop.java
+++ src/test/java/com/rhcloud/test/forloop/ForLoop.java
@@ -10,12 +10,13 @@ import static org.hamcrest.CoreMatchers.*;
  * These tests show that the post increment OR pre increment operators do not make a difference in for loops when added in the third statement.
  * This is because the increment part (third statement in for loop -- after second semicolon) is run after the loop is run first.
  * 
- * 1. So First Initialize {@code i}
- * 2. Check if Condition matches {@code (i < 10)}
- * 3. Run For loop body 
- * 4. Increment
- * 5. Run from 2 to 4 till 2 return false.
- * 
+ * <ol>
+ * <li>So First Initialize {@code int i}</li>
+ * <li>Check Condition {@code if (i < 10)} then proceed </li>
+ * <li>Run For loop body </li>
+ * <li>Increment {@code i++ OR ++i} no difference as value is not currently assigned.</li>
+ * <li>Run from 2 to 4 till 2 returns {@code false}.</li>
+ * </ol>
  * @author Guruprasad Kulkarni <guru@linux.com>
  */
 public class ForLoop {
