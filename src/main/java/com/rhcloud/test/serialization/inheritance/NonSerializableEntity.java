9a0b0ba947e087cb967dcd684cf4590c3409aa0d
Guruprasad Kulkarni <guru@linux.com>
2016-06-23 10:03:29 +0530

    Started to add Serializable Non Serializable Exception example

---
+++ src/main/java/com/rhcloud/test/serialization/inheritance/NonSerializableEntity.java
@@ -0,0 +1,29 @@
+package com.rhcloud.test.serialization.inheritance;
+
+/**
+ * This class is not {@linkplain Serializable}
+ * @author Guruprasad Kulkarni <guru@linux.com>
+ */
+public class NonSerializableEntity {
+    private String objectFromNonSerializableClass;
+    
+    private int primitiveFromNonSerializableClass;
+
+    public String getObjectFromNonSerializableClass() {
+        return objectFromNonSerializableClass;
+    }
+
+    public void setObjectFromNonSerializableClass(String objectFromNonSerializableClass) {
+        this.objectFromNonSerializableClass = objectFromNonSerializableClass;
+    }
+
+    public int getPrimitiveFromNonSerializableClass() {
+        return primitiveFromNonSerializableClass;
+    }
+
+    public void setPrimitiveFromNonSerializableClass(int primitiveFromNonSerializableClass) {
+        this.primitiveFromNonSerializableClass = primitiveFromNonSerializableClass;
+    }
+    
+    
+}
