982f68766798b7608f345a36beb0b5849c9a4a7d
Guruprasad Kulkarni <guru@linux.com>
2017-01-21 12:18:39 +0100

    File readers using new Paths and Files further updates

--- src/main/java/com/linux/test/file/readers/LineFileReader.java
+++ src/main/java/com/linux/test/file/readers/LineFileReader.java
@@ -1,8 +1,18 @@
 package com.linux.test.file.readers;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
@@ -19,8 +29,16 @@ public class LineFileReader {
         this.path = path;
     }
     
-    public static LineFileReader getinstance(Path path) {
+    private LineFileReader() {
+        this(null);
+    }
+    
+    public static LineFileReader getInstance(Path path) {
         return new LineFileReader(path);
+    }
+    
+    public static LineFileReader getInstance() {
+        return new LineFileReader();
     }
     
     public Stream<String> getLines() {
@@ -30,6 +48,27 @@ public class LineFileReader {
             Logger.getLogger(LineFileReader.class.getName()).log(Level.SEVERE, null, ex);
         }
         return Stream.empty();
+    }
+    
+    public List<String> getLines(String filePath) {
+        
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            List<String> fileLines = new ArrayList<>();
+            
+            String readLine;
+            do {
+                readLine = br.readLine();
+                if (null != readLine) {
+                    fileLines.add(readLine);
+                }
+            } while (readLine != null);
+            
+            return fileLines;
+        } catch (IOException ex) {
+            Logger.getLogger(LineFileReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return Collections.emptyList();
     }
     
 }
--- src/test/java/com/linux/test/file/readers/FileReadertest.java
+++ src/test/java/com/linux/test/file/readers/FileReadertest.java
@@ -1,21 +1,43 @@
 package com.linux.test.file.readers;
 
+import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.Test;
 
 /**
- *
+ * Unit tests to learn / remember reading files.
+ * 
  * @author comdotlinux
  */
 public class FileReadertest {
+    
+    private static final String INPUT_FILE_RELATIVE_PATH = "src/test/resources/com/linux/file/readers/testFile";
 
     @Test
     public void readlinesTest() throws URISyntaxException {
-        URI uri = new URI("file://testFile");
-        LineFileReader.getinstance(Paths.get(uri)).getLines().forEach(System.out::print);
+        Path path = Paths.get(new File(INPUT_FILE_RELATIVE_PATH).toURI());
+        Stream<String> stream = LineFileReader.getInstance(path).getLines();
+        List<String> lines = stream.peek(System.out::println).collect(Collectors.toList());
+        
+        assertThat(lines.size(), is(5));
 
     }
+    
+    @Test
+    public void readFileUsingbufferedReader() {
+        List<String> lines = LineFileReader.getInstance().getLines(INPUT_FILE_RELATIVE_PATH);
+        
+        lines.stream().forEach(System.out::println);
+        assertThat(lines.size(), is(5));
+    }
+    
 
 }
